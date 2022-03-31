101001c67d827960391fad48a564c68d9e8ce333
Ijazliaqat <ijazierpu@gmail.com>
2022-03-31 17:06:24 +0500

    add to cart function

--- src/app/shops/shops.component.ts
+++ src/app/shops/shops.component.ts
@@ -19,30 +19,30 @@ export class ShopsComponent implements OnInit {
     {
       productId: 1,
       img:"../../assets/imgs/oring.webp",
-      productName: "Yougurt",
+      productName: "Orange",
       amount: "$10",
-      qnt: 1
+      qnt: 0
     },
     {
       productId: 2,
       img:"../../assets/imgs/Peppers.png",
-      productName: "Yougurt",
+      productName: "Peppers",
       amount: "$10",
-      qnt:1
+      qnt:0
     },
     {
       productId: 3,
       img:"../../assets/imgs/product.webp",
       productName: "Yougurt",
       amount: "$10",
-      qnt: 1
+      qnt: 0
     },
     {
       productId: 4,
       img:"../../assets/imgs/product2.webp",
-      productName: "Yougurt",
+      productName: "Product",
       amount: "$10",
-      qnt: 1
+      qnt: 0
     }
   ];
 
@@ -67,9 +67,49 @@ export class ShopsComponent implements OnInit {
     
   };
 
-  addCart(catagory:any){
+  itemsCart:any = [];
+
+  addCart(category:any){
     // console.log(catagory);
 
-    localStorage.setItem('localCart', JSON.stringify(catagory));
+    let cartDataNull= localStorage.getItem('localCart');
+
+    if (cartDataNull == null){
+
+      let storeDataGet:any = [];
+
+      storeDataGet.push(category);
+
+      localStorage.setItem('localCart', JSON.stringify(storeDataGet));
+
+    } else {
+
+      var id = category.productId;
+
+      let index:number = -1;
+
+      this.itemsCart = JSON.parse(localStorage.getItem('localCart') || '{}');
+
+      for (let i=0; i<this.itemsCart.length; i++){
+        if (parseInt(id) === parseInt(this.itemsCart[i].productId)){
+
+          this.itemsCart[i].qnt = category.qnt;
+
+          index = i;
+
+          break;
+
+        }
+      }
+      if(index == -1){
+        this.itemsCart.push(category);
+        localStorage.setItem('localCart', JSON.stringify(this.itemsCart));
+      }else{
+        localStorage.setItem('localCart', JSON.stringify(this.itemsCart));
+      }
+
+    }
+
+    localStorage.setItem('localCart', JSON.stringify(category));
   };
 }
